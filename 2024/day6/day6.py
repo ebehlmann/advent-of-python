051e6c9950ea1846f74f45b9682035c817c8209c
Emily Behlmann <emily@voteamerica.com>
2024-12-11 21:56:31 -0600

    ugly day 6 part 2

--- 2024/day6/day6.py
+++ 2024/day6/day6.py
@@ -1,3 +1,6 @@
+NEXT_DIRS = {'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'}
+
+
 def process_input(filename):
     grid = []
     guard = None
@@ -22,20 +25,55 @@ def get_next_pos(current_pos, direction):
 
 
 def map_guard_route(grid, pos, direction):
-    next_dirs = {'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'}
     visited = [pos]
     while True:
         next_pos = get_next_pos(pos, direction)
         if next_pos[0] < 0 or next_pos[0] >= len(grid[0]) or next_pos[1] < 0 or next_pos[1] >= len(grid):
             break
         elif grid[next_pos[1]][next_pos[0]] == '#':
-            direction = next_dirs[direction]
+            direction = NEXT_DIRS[direction]
         else:
             if next_pos not in visited:
                 visited.append(next_pos)
             pos = next_pos
-    return len(visited)
+    return visited
+
+
+def is_looped(route):
+    visited = route.copy()
+    current = visited.pop()
+    if current in visited and len(visited) > 1:
+        last_occurrence = len(visited) - 1 - visited[::-1].index(current)
+        if last_occurrence != 0:
+            if visited[-1] == visited[last_occurrence-1]:
+                return True
+    return False
+
+
+def test_obstructions(grid, pos, direction, possible_obstructions):
+    successful_obstructions = 0
+    for obstruction in possible_obstructions:
+        if obstruction != pos:
+            route = [pos]
+            current_pos = pos
+            current_direction = direction
+            while True:
+                next_pos = get_next_pos(current_pos, current_direction)
+                if next_pos[0] < 0 or next_pos[0] >= len(grid[0]) or next_pos[1] < 0 or next_pos[1] >= len(grid):
+                    break
+                elif grid[next_pos[1]][next_pos[0]] == '#' or next_pos == obstruction:
+                    current_direction = NEXT_DIRS[current_direction]
+                else:
+                    current_pos = next_pos
+                    route.append(current_pos)
+                    if is_looped(route):
+                        successful_obstructions += 1
+                        break
+    return successful_obstructions
+
 
 
 grid_layout, guard_pos = process_input('input.txt')
-print(f'Part 1: {map_guard_route(grid_layout, guard_pos, "N")}')
+visited = map_guard_route(grid_layout, guard_pos, "N")
+print(f'Part 1: {len(visited)}')
+print(f'Part 2: {test_obstructions(grid_layout, guard_pos, "N", visited)}')
